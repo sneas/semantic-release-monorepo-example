3a1c4f62b3cd39dce9f56ee90064088c30e765ca
Dima Snisarenko <snisarenkodima@gmail.com>
2021-12-30 06:26:05 +0100

    ci: add semantic release config

---
+++ packages/b/.releaserc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ci: false,
+  extends: 'semantic-release-monorepo',
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ]
+  ],
+};
